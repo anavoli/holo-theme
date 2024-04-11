6c3bd4fb26d6930de1446e5a54a8b9b3f9a5064f
ana <105382108+anavoli@users.noreply.github.com>
2024-04-11 18:10:22 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Neško Đorić`,
     description: `Ponešto o Nešku Đoriću`,
-    siteUrl: "https://djore.netlify.app//",
+    siteUrl: "https://ndjore.netlify.app//",
     author: `Neško.xyz`,
   },
   plugins: [
