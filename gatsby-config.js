739cecadfdffb1f3e37b34bbe72d7f2904353bc4
ana <105382108+anavoli@users.noreply.github.com>
2024-04-11 17:42:37 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Holo - Gatsby Starter`,
-    description: `Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS`,
-    siteUrl: "https://holo-theme.netlify.app/",
-    author: `Travis Lord <hi@travislord.xyz>`,
+    title: `Neško Đorić`,
+    description: `Ponešto o Nešku Đoriću`,
+    siteUrl: "https://djore.netlify.app//",
+    author: `Neško.xyz`,
   },
   plugins: [
     `gatsby-plugin-image`,
